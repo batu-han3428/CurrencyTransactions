5e8f3e47256751e5059758a5428d244cf0a55384
batu-han3428 <batu_6407@hotmail.com.tr>
2021-11-26 23:26:33 +0300

    Şifre sıfırlama sayfası tasarımı

---
+++ dövizAlimSatim/Views/Account/ResetPassword.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace dövizAlimSatim.Views.Account
+{
+    partial class ResetPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResetPassword));
+            this.grplogin = new System.Windows.Forms.GroupBox();
+            this.lblwarning = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btngiris = new System.Windows.Forms.Button();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.grplogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grplogin
+            // 
+            this.grplogin.BackColor = System.Drawing.Color.Transparent;
+            this.grplogin.Controls.Add(this.label2);
+            this.grplogin.Controls.Add(this.lblwarning);
+            this.grplogin.Controls.Add(this.pictureBox1);
+            this.grplogin.Controls.Add(this.btngiris);
+            this.grplogin.Controls.Add(this.txtmail);
+            this.grplogin.Controls.Add(this.label1);
+            this.grplogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grplogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grplogin.ForeColor = System.Drawing.Color.White;
+            this.grplogin.Location = new System.Drawing.Point(12, 18);
+            this.grplogin.Name = "grplogin";
+            this.grplogin.Size = new System.Drawing.Size(327, 415);
+            this.grplogin.TabIndex = 13;
+            this.grplogin.TabStop = false;
+            this.grplogin.Text = "Şifremi Sıfırla";
+            // 
+            // lblwarning
+            // 
+            this.lblwarning.AutoSize = true;
+            this.lblwarning.BackColor = System.Drawing.Color.Transparent;
+            this.lblwarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblwarning.Location = new System.Drawing.Point(9, 35);
+            this.lblwarning.Name = "lblwarning";
+            this.lblwarning.Size = new System.Drawing.Size(0, 20);
+            this.lblwarning.TabIndex = 19;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(278, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox1, "Geri");
+            // 
+            // btngiris
+            // 
+            this.btngiris.BackColor = System.Drawing.Color.Transparent;
+            this.btngiris.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btngiris.ForeColor = System.Drawing.Color.Brown;
+            this.btngiris.Location = new System.Drawing.Point(18, 267);
+            this.btngiris.Name = "btngiris";
+            this.btngiris.Size = new System.Drawing.Size(291, 32);
+            this.btngiris.TabIndex = 16;
+            this.btngiris.Text = "Kodu Gönder";
+            this.btngiris.UseVisualStyleBackColor = false;
+            // 
+            // txtmail
+            // 
+            this.txtmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtmail.Location = new System.Drawing.Point(121, 195);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(188, 27);
+            this.txtmail.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("SimSun-ExtB", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(14, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "E-Mail:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 25);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Kayıtlı mail adresinizi giriniz..";
+            // 
+            // ResetPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(350, 450);
+            this.Controls.Add(this.grplogin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ResetPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Şifre Sıfırla";
+            this.grplogin.ResumeLayout(false);
+            this.grplogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grplogin;
+        private System.Windows.Forms.Label lblwarning;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btngiris;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
