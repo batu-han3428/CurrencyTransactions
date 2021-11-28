c5928c564118d20f1d8516173ed3645564cc0df5
batu-han3428 <batu_6407@hotmail.com.tr>
2021-11-28 11:04:17 +0300

    kullanıcı login oldukan sonra ki index tasarımı yapıldı

--- dövizAlimSatim/Views/Home/Index.Designer.cs
+++ dövizAlimSatim/Views/Home/Index.Designer.cs
@@ -42,7 +42,22 @@ namespace CurrencyTransactions
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
             this.btngiris = new System.Windows.Forms.Button();
+            this.lblwelcome = new System.Windows.Forms.Label();
+            this.pctrbxunlogin = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.grptrade = new System.Windows.Forms.GroupBox();
+            this.cmbxcurrency = new System.Windows.Forms.ComboBox();
+            this.rbtnbuy = new System.Windows.Forms.RadioButton();
+            this.rbtnsell = new System.Windows.Forms.RadioButton();
+            this.txtquantity = new System.Windows.Forms.TextBox();
+            this.btnprocess = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblbalance = new System.Windows.Forms.Label();
+            this.btninvestments = new System.Windows.Forms.Button();
+            this.btnaccountmovement = new System.Windows.Forms.Button();
             this.grpkur.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxunlogin)).BeginInit();
+            this.grptrade.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -61,7 +76,7 @@ namespace CurrencyTransactions
             this.grpkur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.grpkur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.grpkur.ForeColor = System.Drawing.Color.White;
-            this.grpkur.Location = new System.Drawing.Point(12, 38);
+            this.grpkur.Location = new System.Drawing.Point(12, 48);
             this.grpkur.Name = "grpkur";
             this.grpkur.Size = new System.Drawing.Size(327, 210);
             this.grpkur.TabIndex = 11;
@@ -177,6 +192,161 @@ namespace CurrencyTransactions
             this.btngiris.UseVisualStyleBackColor = false;
             this.btngiris.Click += new System.EventHandler(this.btngiris_Click);
             // 
+            // lblwelcome
+            // 
+            this.lblwelcome.AutoSize = true;
+            this.lblwelcome.BackColor = System.Drawing.Color.Transparent;
+            this.lblwelcome.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblwelcome.ForeColor = System.Drawing.Color.White;
+            this.lblwelcome.Location = new System.Drawing.Point(12, 17);
+            this.lblwelcome.Name = "lblwelcome";
+            this.lblwelcome.Size = new System.Drawing.Size(77, 14);
+            this.lblwelcome.TabIndex = 15;
+            this.lblwelcome.Text = "Hoşgeldin";
+            this.lblwelcome.Visible = false;
+            // 
+            // pctrbxunlogin
+            // 
+            this.pctrbxunlogin.BackColor = System.Drawing.Color.Transparent;
+            this.pctrbxunlogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pctrbxunlogin.Image = ((System.Drawing.Image)(resources.GetObject("pctrbxunlogin.Image")));
+            this.pctrbxunlogin.Location = new System.Drawing.Point(296, 5);
+            this.pctrbxunlogin.Name = "pctrbxunlogin";
+            this.pctrbxunlogin.Size = new System.Drawing.Size(43, 40);
+            this.pctrbxunlogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctrbxunlogin.TabIndex = 18;
+            this.pctrbxunlogin.TabStop = false;
+            this.toolTip1.SetToolTip(this.pctrbxunlogin, "Çıkış");
+            this.pctrbxunlogin.Visible = false;
+            // 
+            // grptrade
+            // 
+            this.grptrade.BackColor = System.Drawing.Color.Transparent;
+            this.grptrade.Controls.Add(this.btnaccountmovement);
+            this.grptrade.Controls.Add(this.btninvestments);
+            this.grptrade.Controls.Add(this.lblbalance);
+            this.grptrade.Controls.Add(this.label3);
+            this.grptrade.Controls.Add(this.btnprocess);
+            this.grptrade.Controls.Add(this.txtquantity);
+            this.grptrade.Controls.Add(this.rbtnsell);
+            this.grptrade.Controls.Add(this.rbtnbuy);
+            this.grptrade.Controls.Add(this.cmbxcurrency);
+            this.grptrade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grptrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grptrade.ForeColor = System.Drawing.Color.White;
+            this.grptrade.Location = new System.Drawing.Point(12, 282);
+            this.grptrade.Name = "grptrade";
+            this.grptrade.Size = new System.Drawing.Size(327, 202);
+            this.grptrade.TabIndex = 19;
+            this.grptrade.TabStop = false;
+            this.grptrade.Text = "Kolay Al/Sat";
+            this.grptrade.Visible = false;
+            // 
+            // cmbxcurrency
+            // 
+            this.cmbxcurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbxcurrency.FormattingEnabled = true;
+            this.cmbxcurrency.Location = new System.Drawing.Point(29, 84);
+            this.cmbxcurrency.Name = "cmbxcurrency";
+            this.cmbxcurrency.Size = new System.Drawing.Size(97, 24);
+            this.cmbxcurrency.TabIndex = 0;
+            // 
+            // rbtnbuy
+            // 
+            this.rbtnbuy.AutoSize = true;
+            this.rbtnbuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtnbuy.Location = new System.Drawing.Point(29, 50);
+            this.rbtnbuy.Name = "rbtnbuy";
+            this.rbtnbuy.Size = new System.Drawing.Size(41, 21);
+            this.rbtnbuy.TabIndex = 1;
+            this.rbtnbuy.TabStop = true;
+            this.rbtnbuy.Text = "Al";
+            this.rbtnbuy.UseVisualStyleBackColor = true;
+            // 
+            // rbtnsell
+            // 
+            this.rbtnsell.AutoSize = true;
+            this.rbtnsell.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.rbtnsell.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtnsell.Location = new System.Drawing.Point(76, 50);
+            this.rbtnsell.Name = "rbtnsell";
+            this.rbtnsell.Size = new System.Drawing.Size(50, 21);
+            this.rbtnsell.TabIndex = 2;
+            this.rbtnsell.TabStop = true;
+            this.rbtnsell.Text = "Sat";
+            this.rbtnsell.UseVisualStyleBackColor = true;
+            // 
+            // txtquantity
+            // 
+            this.txtquantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtquantity.Location = new System.Drawing.Point(29, 121);
+            this.txtquantity.Name = "txtquantity";
+            this.txtquantity.Size = new System.Drawing.Size(97, 22);
+            this.txtquantity.TabIndex = 3;
+            // 
+            // btnprocess
+            // 
+            this.btnprocess.BackColor = System.Drawing.Color.Transparent;
+            this.btnprocess.Font = new System.Drawing.Font("Yu Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnprocess.ForeColor = System.Drawing.Color.Brown;
+            this.btnprocess.Location = new System.Drawing.Point(29, 153);
+            this.btnprocess.Name = "btnprocess";
+            this.btnprocess.Size = new System.Drawing.Size(97, 24);
+            this.btnprocess.TabIndex = 17;
+            this.btnprocess.Text = "İşlem";
+            this.btnprocess.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(192, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 14);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Cüzdan:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Visible = false;
+            // 
+            // lblbalance
+            // 
+            this.lblbalance.AutoSize = true;
+            this.lblbalance.BackColor = System.Drawing.Color.Transparent;
+            this.lblbalance.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblbalance.ForeColor = System.Drawing.Color.White;
+            this.lblbalance.Location = new System.Drawing.Point(258, 55);
+            this.lblbalance.Name = "lblbalance";
+            this.lblbalance.Size = new System.Drawing.Size(55, 14);
+            this.lblbalance.TabIndex = 19;
+            this.lblbalance.Text = "Bakiye";
+            this.lblbalance.Visible = false;
+            // 
+            // btninvestments
+            // 
+            this.btninvestments.BackColor = System.Drawing.Color.Transparent;
+            this.btninvestments.Font = new System.Drawing.Font("Yu Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btninvestments.ForeColor = System.Drawing.Color.Brown;
+            this.btninvestments.Location = new System.Drawing.Point(196, 97);
+            this.btninvestments.Name = "btninvestments";
+            this.btninvestments.Size = new System.Drawing.Size(113, 24);
+            this.btninvestments.TabIndex = 20;
+            this.btninvestments.Text = "Yatırımlarım";
+            this.btninvestments.UseVisualStyleBackColor = false;
+            // 
+            // btnaccountmovement
+            // 
+            this.btnaccountmovement.BackColor = System.Drawing.Color.Transparent;
+            this.btnaccountmovement.Font = new System.Drawing.Font("Yu Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnaccountmovement.ForeColor = System.Drawing.Color.Brown;
+            this.btnaccountmovement.Location = new System.Drawing.Point(196, 128);
+            this.btnaccountmovement.Name = "btnaccountmovement";
+            this.btnaccountmovement.Size = new System.Drawing.Size(112, 49);
+            this.btnaccountmovement.TabIndex = 21;
+            this.btnaccountmovement.Text = "Hesap Hareketi";
+            this.btnaccountmovement.UseVisualStyleBackColor = false;
+            // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -184,7 +354,10 @@ namespace CurrencyTransactions
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(351, 450);
+            this.ClientSize = new System.Drawing.Size(351, 496);
+            this.Controls.Add(this.grptrade);
+            this.Controls.Add(this.pctrbxunlogin);
+            this.Controls.Add(this.lblwelcome);
             this.Controls.Add(this.btngiris);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.linkLabel1);
@@ -199,6 +372,9 @@ namespace CurrencyTransactions
             this.Load += new System.EventHandler(this.Form1_Load);
             this.grpkur.ResumeLayout(false);
             this.grpkur.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxunlogin)).EndInit();
+            this.grptrade.ResumeLayout(false);
+            this.grptrade.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,6 +393,19 @@ namespace CurrencyTransactions
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btngiris;
+        private System.Windows.Forms.Label lblwelcome;
+        private System.Windows.Forms.PictureBox pctrbxunlogin;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.GroupBox grptrade;
+        private System.Windows.Forms.TextBox txtquantity;
+        private System.Windows.Forms.RadioButton rbtnsell;
+        private System.Windows.Forms.RadioButton rbtnbuy;
+        private System.Windows.Forms.ComboBox cmbxcurrency;
+        private System.Windows.Forms.Button btnaccountmovement;
+        private System.Windows.Forms.Button btninvestments;
+        private System.Windows.Forms.Label lblbalance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnprocess;
     }
 }
 
